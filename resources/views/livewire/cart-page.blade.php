daed80fb588678061112ad8bf8ba846dfe7ab153
unknown <lemuelmangao04@gmail.com>
2025-12-02 00:47:19 +0800

    Chapter 19

--- resources/views/livewire/cart-page.blade.php
+++ resources/views/livewire/cart-page.blade.php
@@ -72,7 +72,9 @@
             <span class="font-semibold">{{ Number::currency($grand_total, 'INR') }}</span>
           </div>
           @if($cart_items)
-          <button class="bg-blue-500 text-white py-2 px-4 rounded-lg mt-4 w-full">Checkout</button>
+          <div>
+            <a href="/checkout" class="bg-blue-500 block text-center text-white py-2 px-4 rounded-lg mt-4 w-full">Checkout</a>
+          </div>
           @endif
         </div>
 
